8ed1990ec92e8921cd053e697cac511a45c8c6e5
hyun <h.zoo3963>
2023-06-28 11:44:02 +0900

    commit 최종

--- src/main/java/com/sparta/testpost/controller/PostController.java
+++ src/main/java/com/sparta/testpost/controller/PostController.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
